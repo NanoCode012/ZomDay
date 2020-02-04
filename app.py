2ed3435f6d7480b62d952987b3b66a0a03fe74b4
NanoCode012 <kevinvong@rocketmail.com>
2020-02-04 17:06:29 +0700

    start to test return message

---
+++ app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from flask import Flask, jsonify, render_template, request
+from flask_sqlacademy import SQLAcademy
+import json
+import numpy as np
+
+
+#import pymongo
+#from pymongo import MongoClient
+#from flask_restful import Resource, Api, reqparse
+
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
+app = Flask(__name__)
+app.config("SQLALCHEMY_DATABASE_URI") = r"mysql://id10391787_acclog:7%4dx7SC%to$@localhost/id10391787_accountlogin"
+db = SQLAcademy(app)
+
+lineaccesstoken = 'tIusZcDlB46unf8x8Lr2WkL197uMfnW7UJ85O021hxTVJ1p8/IRzGUDrew7vS7H4589SiLf6wlALp+JMm93AGUv6X0D739fynRQoPMtcFSie+MlcWOgexoVuhuhglLwgVxl/um3ewL3vlaXKPQQDYQdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
+
+####################### new ########################
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_line = request.get_json(force=False,cache=False)
+    return json_line
+
+    # json_line = json.dumps(json_line)
+    # decoded = json.loads(json_line)
+
+
+    # no_event = len(decoded['events'])
+    # for i in range(no_event):
+    #     event = decoded['events'][i]
+    #     #try:
+    #     event_handle(event)
+    #     #except:
+    #     #    pass
+    return '',200
+
+
+def event_handle(event):
+    return ""
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
