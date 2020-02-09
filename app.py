66a5ae52e39ca75c44d72e170793967a886dbebb
NanoCode012 <kevinvong@rocketmail.com>
2020-02-09 10:27:58 +0700

    test flex message

--- app.py
+++ app.py
@@ -186,11 +186,196 @@ def options():
 #         return jsonify({'message' : 'error'})
 #     return jsonify({"message" : msg})
 
+@app.route("/fxmessage", methods=["GET"])
+def fxmessage():
+    return jsonify({
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "carousel",
+    "contents": [
+      {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+          "size": "full",
+          "aspectRatio": "20:13",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "text",
+              "text": "Arm Chair, White",
+              "size": "xl",
+              "weight": "bold",
+              "wrap": True
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "$49",
+                  "flex": 0,
+                  "size": "xl",
+                  "weight": "bold",
+                  "wrap": True
+                },
+                {
+                  "type": "text",
+                  "text": ".99",
+                  "flex": 0,
+                  "size": "sm",
+                  "weight": "bold",
+                  "wrap": True
+                }
+              ]
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Add to Cart",
+                "uri": "https://linecorp.com"
+              },
+              "style": "primary"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Add to whishlist",
+                "uri": "https://linecorp.com"
+              }
+            }
+          ]
+        }
+      },
+      {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
+          "size": "full",
+          "aspectRatio": "20:13",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "text",
+              "text": "Metal Desk Lamp",
+              "size": "xl",
+              "weight": "bold",
+              "wrap": True
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "flex": 1,
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "$11",
+                  "flex": 0,
+                  "size": "xl",
+                  "weight": "bold",
+                  "wrap": True
+                },
+                {
+                  "type": "text",
+                  "text": ".99",
+                  "flex": 0,
+                  "size": "sm",
+                  "weight": "bold",
+                  "wrap": True
+                }
+              ]
+            },
+            {
+              "type": "text",
+              "text": "Temporarily out of stock",
+              "flex": 0,
+              "margin": "md",
+              "size": "xxs",
+              "color": "#FF5551",
+              "wrap": True
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Add to Cart",
+                "uri": "https://linecorp.com"
+              },
+              "flex": 2,
+              "color": "#AAAAAA",
+              "style": "primary"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Add to wish list",
+                "uri": "https://linecorp.com"
+              }
+            }
+          ]
+        }
+      },
+      {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "See more",
+                "uri": "https://linecorp.com"
+              },
+              "flex": 1,
+              "gravity": "center"
+            }
+          ]
+        }
+      }
+    ]
+  }
+})
+
 # def game_reset():
 #     game_start = False
 #     current_level = -1
 #     food = 2
 #     options = ["", ""]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
